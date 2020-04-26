953759951d933e907f136e7d11650e9bdf270bff
Karson Nichols <nicholsk18@gmail.com>
2020-04-25 22:33:29 -0500

    testing

--- src/Details.js
+++ src/Details.js
@@ -16,28 +16,29 @@ class Details extends React.Component {
     // }
 
     // replaces above
-    state = { loading: true, showModal: false };
 
+    state = { loading: true, showModal: false };
     componentDidMount() {
-        pet.animal(this.props.id).then(({ animal }) => {
-            this.setState({
-                url: animal.url,
-                name: animal.name,
-                animal: animal.type,
-                location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-                description: animal.description,
-                media: animal.photos,
-                breed: animal.breeds.primary,
-                loading: false,
-            });
-        }, console.error);
+        pet.animal(this.props.id)
+            .then(({ animal }) => {
+                this.setState({
+                    name: animal.name,
+                    animal: animal.type,
+                    location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+                    description: animal.description,
+                    media: animal.photos,
+                    breed: animal.breeds.primary,
+                    url: animal.url,
+                    loading: false,
+                });
+            })
+            .catch((err) => this.setState({ error: err }));
     }
     toggleModal = () => this.setState({ showModal: !this.state.showModal });
-    adopt = () => navigate(this.state.url); // can use redirect
-
+    adopt = () => navigate(this.state.url);
     render() {
         if (this.state.loading) {
-            return <h1>loading ...</h1>;
+            return <h1>loading … </h1>;
         }
 
         const {
@@ -45,8 +46,8 @@ class Details extends React.Component {
             breed,
             location,
             description,
-            name,
             media,
+            name,
             showModal,
         } = this.state;
 
@@ -55,12 +56,12 @@ class Details extends React.Component {
                 <Carousel media={media} />
                 <div>
                     <h1>{name}</h1>
-                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
+                    <h2>{`${animal} — ${breed} — ${location}`}</h2>
                     <ThemeContext.Consumer>
-                        {(themeHook) => (
+                        {([theme]) => (
                             <button
+                                style={{ backgroundColor: theme }}
                                 onClick={this.toggleModal}
-                                style={{ backgroundColor: themeHook[0] }}
                             >
                                 Adopt {name}
                             </button>
@@ -69,14 +70,10 @@ class Details extends React.Component {
                     <p>{description}</p>
                     {showModal ? (
                         <Modal>
-                            <div>
-                                <h1>Would you like to adopt {name}</h1>
-                                <div className="buttons">
-                                    <button onClick={this.adopt}>Yes</button>
-                                    <button onClick={this.toggleModal}>
-                                        No, I am a monster
-                                    </button>
-                                </div>
+                            <h1>Would you like to adopt {name}?</h1>
+                            <div className="buttons">
+                                <button onClick={this.adopt}>Yes</button>
+                                <button onClick={this.toggleModal}>No</button>
                             </div>
                         </Modal>
                     ) : null}
@@ -86,7 +83,7 @@ class Details extends React.Component {
     }
 }
 
-export default function DetailsWithErrorBoundary(props) {
+export default function DetailsErrorBoundary(props) {
     return (
         <ErrorBoundary>
             <Details {...props} />
--- src/Results.js
+++ src/Results.js
@@ -3,21 +3,23 @@ import Pet from "./Pet";
 
 const Results = ({ pets }) => {
     return (
-        <div className="search">
-            {pets.length === 0 ? (
-                <h1>No Pets Fount</h1>
+        <div data-testid="search-results" className="search">
+            {!pets.length ? (
+                <h1>No Pets Found</h1>
             ) : (
-                pets.map((pet) => (
-                    <Pet
-                        animal={pet.type}
-                        key={pet.id}
-                        name={pet.name}
-                        breed={pet.breeds.primary}
-                        media={pet.photos}
-                        location={`${pet.contact.address.city}, ${pet.contact.address.state}`}
-                        id={pet.id}
-                    />
-                ))
+                pets.map((pet) => {
+                    return (
+                        <Pet
+                            animal={pet.type}
+                            key={pet.id}
+                            name={pet.name}
+                            breed={pet.breeds.primary}
+                            media={pet.photos}
+                            location={`${pet.contact.address.city}, ${pet.contact.address.state}`}
+                            id={pet.id}
+                        />
+                    );
+                })
             )}
         </div>
     );
